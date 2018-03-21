0682859156972d4442bc34ed289dca4da5f49fcd
kmdtmyk <winback_banderas@yahoo.co.jp>
2018-03-21 22:13:51 +0900

    Install jest

--- src/npm-template.js
+++ src/npm-template.js
@@ -1,3 +1,7 @@
 export default class{
 
+  static add(value1, value2){
+    return value1 + value2
+  }
+
 }
---
+++ src/npm-template.test.js
@@ -0,0 +1,5 @@
+import NpmTemplate from './npm-template'
+
+it('add', () => {
+  expect(NpmTemplate.add(1, 2)).toBe(3)
+})
